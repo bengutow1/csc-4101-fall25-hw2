46574fb460dc41d5a1dbce148d3d06c25daa84f4
bgutow1 <hyprr@seymore.localdomain>
2025-09-28 09:55:39 -0500

    Had to fix the linking of OUnit in both student and public test files, also finished part 1 of homework

--- src/basics.ml
+++ src/basics.ml
@@ -6,14 +6,39 @@ let abs x =
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
 
-let rev_tup (tup : 'a * 'b) = failwith "unimplemented"
-let rev_triple (tup : 'a * 'b * 'c) = failwith "unimplemented"
+let rev_tup ((a,b) : 'a * 'b) = (b, a);;
+let rev_triple ((a, b, c) : 'a * 'b * 'c) = (c, b, a);;
 
-let is_odd x = failwith "unimplemented"
+let is_odd x =
+        if x mod 2 = 0 then
+                false
+        else
+                true
+;;
 
-let is_older (date1: int * int * int) (date2: int * int * int) = failwith "unimplemented"
+let is_older (date1: int * int * int) (date2: int * int * int) =
+        let (y1, m1, d1) = date1 in
+        let (y2, m2, d2) = date2 in
+        if y1 <= y2 then
+                if y1 < y2 then
+                        true
+                else
+                        if m1 <= m2 then
+                                if m1 < m2 then
+                                        true
+                                else
+                                        if d1 < d2 then
+                                                true
+                                        else
+                                                false
+                        else
+                                false
+        else
+                false
+;;
 
-let to_us_format (date1: int * int * int) = failwith "unimplemented"
+let to_us_format ((year, month, day): int * int * int) = (month, day, year);;
+        
 (*******************************)
 (* Part 2: Recursive Functions *)
 (*******************************)
